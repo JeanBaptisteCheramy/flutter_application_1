cc18fda0d1e1c77aaf0c0823d6aec83c0363f4c0
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-05-10 17:02:10 +0200

    button colors (switch)

--- lib/widgets/calc_button.dart
+++ lib/widgets/calc_button.dart
@@ -11,9 +11,28 @@ class CalcButton extends StatelessWidget {
     logger.d(data);
   }
 
+  Color handleColor(String data) {
+    switch (data) {
+      case "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9":
+        return const Color.fromARGB(255, 255, 254, 245);
+      case "AC" || "<" || "=":
+        return const Color.fromARGB(255, 69, 94, 64);
+      case "%" || "√" || "/" || "x" || "-" || "+" || ",":
+        return const Color.fromARGB(255, 180, 176, 100);
+      default:
+        return const Color.fromARGB(255, 255, 254, 245);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 50, child: ElevatedButton(onPressed: test, child: Text(data)));
+        height: 50,
+        child: ElevatedButton(
+            onPressed: test,
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll<Color>(handleColor(data))),
+            child: Text(data)));
   }
 }
